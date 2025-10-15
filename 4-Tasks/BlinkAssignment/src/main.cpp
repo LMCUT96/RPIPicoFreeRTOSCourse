b30a970faca236c983f2bf99017aa6b9e174496e
LMCUT96 <leysonyaba@gmail.com>
2025-10-15 10:42:33 +0800

    Update main.cpp

--- 4-Tasks/BlinkAssignment/src/main.cpp
+++ 4-Tasks/BlinkAssignment/src/main.cpp
@@ -21,9 +21,9 @@
 #define TASK_PRIORITY		( tskIDLE_PRIORITY + 1UL )
 
 //LED PAD to use
-#define LED_PAD				0
-#define LED1_PAD			2
-#define LED2_PAD			3
+#define LED_PAD				2
+#define LED1_PAD			3
+#define LED2_PAD			4
 
 
 
@@ -122,12 +122,14 @@ int main( void )
 {
 	//Setup serial over USB and give a few seconds to settle before we start
     stdio_init_all();
-    sleep_ms(2000);
+    sleep_ms(3000);
     printf("GO\n");
 
     //Start tasks and scheduler
     const char *rtos_name = "FreeRTOS";
     printf("Starting %s on core 0:\n", rtos_name);
+	printf("YABA-LEY Leyson");
+	printf("F14128811");
     vLaunch();
 
 
